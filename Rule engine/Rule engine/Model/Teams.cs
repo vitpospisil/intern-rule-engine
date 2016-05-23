fa779528ecec216d1884446ea74e1fed856da540
Antonín Drahovzal <antonin.drahovzal@pslib.cz>
2016-05-23 16:01:35 +0200

    intern commit

---
+++ Rule engine/Rule engine/Model/Teams.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rule_engine.Model
+{
+    class Teams
+    {
+        List<string> TeamsList = new List<string>();
+        public Teams()
+        {
+
+        }
+    }
+}
---
+++ Rule engine/Rule engine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rule_engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
