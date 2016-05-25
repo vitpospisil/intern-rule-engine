111da1ad61dec235b582d41da7ac87190088d174
Antonín Drahovzal <antonin.drahovzal@pslib.cz>
2016-05-25 15:02:30 +0200

    CalculatePoints

--- Rule engine/Rule engine/Model/Rules.cs
+++ Rule engine/Rule engine/Model/Rules.cs
@@ -8,25 +8,58 @@ namespace Rule_engine.Model
 {
     public class Rules
     {
+        public static void CalculateRank(List<Team> teams)
+        {
+            int rank = 0;
+            int previousposition = -1;
+            int countOfTeamsWithSameRank = 0;
+            foreach (var f in teams.OrderBy(i => i.Position))
+            {
+                if (f.Position > 0)
+                {
+                    if(f.Position == previousposition)
+                    {
+                        countOfTeamsWithSameRank += 1;
+                    }
+                    if (f.Position != previousposition)
+                    {
+                        rank = rank + 1 + countOfTeamsWithSameRank;
+                        countOfTeamsWithSameRank = 0;
+                    }
+                    f.Rank = rank;
+                }
+                previousposition = f.Position;
+                Console.WriteLine(f.Name + " has rank – " + f.Rank);
+            }
+            Console.WriteLine();
+        }
+
         public static void CalculatePoints(List<Team> teams)
         {
-            int rank = 1;
-
-            foreach(var f in teams.OrderBy(i => i.Position))
+            double previousSamePoints = -1;
+            teams = teams.OrderBy(i => i.Position).ToList();
+            foreach (var p in teams.OrderBy(i => i.Position))
             {
-                if (f.Position != 0)
+                
+                if(p.Points > 0)
                 {
-                    f.Rank = rank;
-                    rank += 1;
-            
-                }
-                while(f.Position != f.Rank)
-                {
-                    f.Rank -= 1;
+                    if(p.Points != previousSamePoints)
+                    {
+                        p.Points += 1;
+                    }
+                    else if(p.Points == previousSamePoints)
+                    {
+                        p.Points = p.Points + 1 + p.Points;
+                        p.Points = p.Points / 2;
+                    }
                 }
 
+                p.Points = p.Rank;
+                previousSamePoints = p.Points;
 
-                Console.WriteLine( f.Name + " has " + f.Points.ToString() + " points");
+                Console.WriteLine(p.Name + " has " + p.Points + " points");
+
+
             }
             Console.WriteLine();
         }
